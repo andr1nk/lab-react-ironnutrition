ef743f3514be937a2243d6fda1b4b02e81846aed
andr1nk <andre@avertu.ch>
2019-05-01 14:10:06 +0200

    search so close

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,6 +5,7 @@ import 'bulma/css/bulma.css';
 import foodsJSON from './foods.json';
 import FoodBox from "./components/FoodBox";
 import FoodForm from "./components/FoodForm";
+import Search from "./components/Search";
 
 
 class App extends Component {
@@ -17,17 +18,36 @@ class App extends Component {
     const foodsCopy = this.state.foods.slice()
     foodsCopy.unshift(food)
 
-    this.setState ({
+    this.setState({
       foods: foodsCopy
     })
+  }
+
+  searchFood = search => {
+    console.log("search is :" + search)
+    const foodsCopy = this.state.foods.slice()
+    
+    foodsCopy.filter(el => {
+      console.log(el.name.includes(search))      
+      el.name.includes(search)
+    })
+    console.log(foodsCopy)
+
+    this.setState({
+      foods: foodsCopy
+    })
+
   }
 
   render() {
     return (
       <div className="App">
-      <div>
-        <FoodForm addFood={this.addFood}/>
-      </div>
+        <div>
+          <FoodForm addFood={this.addFood} />
+        </div>
+        <div>
+          <Search searchFood={this.searchFood} />
+        </div>
         <div className="foodList">
           {this.state.foods.map((food, index) => (
             <FoodBox key={index} food={food} />
--- starter-code/src/components/FoodBox.js
+++ starter-code/src/components/FoodBox.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 export default class FoodBox extends Component {
   render() {
-    console.log(this.props)
     let {food} = this.props
     return (
       <div className="box">
